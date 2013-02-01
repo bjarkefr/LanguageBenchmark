4131d569f5cfa08d275e2438152220dc19f288b4
Bjarke Fr?sig <bjarke@laptop>
2013-02-01 17:55:42 +0100

    Faster...

--- Exp/sudoku.c
+++ Exp/sudoku.c
@@ -15,13 +15,14 @@
 //	rb << nz-bit; ? this is a rotate through nz bit....? possible?
 
 #define div3(N) ((N * 11) >> 5)
+#define div9(N) ((N * 57) >> 9)
 
 struct bitboard
 {
 	unsigned int cols[9];
 	unsigned int rows[9];
 	unsigned int blocks[3][3];
-	unsigned int trace[9][9];
+	unsigned int trace[81];
 } board;
 
 static int bitpos(int n)
@@ -56,33 +57,23 @@ static int bitpos(int n)
 
 static void print_board()
 {
+	int p = 0;
 	for(int y = 0; y < 9; ++y)
 	{
-		for(int x = 0; x < 9; ++x)
-			printf("%i ", bitpos(board.trace[y][x] & 511));
+		for(int x = 0; x < 9; ++x, ++p)
+			printf("%i ", bitpos(board.trace[p] & 511));
 		printf("\n");
 	}
 }
 
-static void process(int x, int y)
+static void process(int p)
 {
-	if(board.trace[y][x] & 512)
+	int y = div9(p);
+	int x = p % 9;
+
+	if(board.trace[p] & 512)
 	{
-		++x;
-		if(x == 9)
-		{
-			x = 0;
-			++y;
-		}
-
-		process(x, y);
-
-		--x;
-		if(x == -1)
-		{
-			x = 9;
-			--y;
-		}
+		process(p + 1);
 		return;
 	}
 
@@ -98,9 +89,9 @@ static void process(int x, int y)
 		if(*cb & v)
 			continue;
 
-		board.trace[y][x] = v;
+		board.trace[p] = v;
 
-		if(y == 8 && x == 8)
+		if(p == 80)
 		{
 			print_board();
 			return;
@@ -110,21 +101,7 @@ static void process(int x, int y)
 		*cy |= v;
 		*cb |= v;
 
-		++x;
-		if(x == 9)
-		{
-			x = 0;
-			++y;
-		}
-
-		process(x, y);
-
-		--x;
-		if(x == -1)
-		{
-			x = 9;
-			--y;
-		}
+		process(p + 1);
 
 		*cb ^= v;
 		*cy ^= v;
@@ -134,60 +111,41 @@ static void process(int x, int y)
 
 static void read_board_from_console()
 {
+	int p = 0;
 	for(int y = 0; y < 9; ++y)
-		for(int x = 0; x < 9; ++x)
+		for(int x = 0; x < 9; ++x, ++p)
 		{
 			unsigned int v;
 			scanf("%u", &v);
-			board.trace[y][x] = v;
+			board.trace[p] = v;
 		}
 }
 
 static void preprocess()
 {
+	int p = 0;
 	for(int y = 0; y < 9; ++y)
 	{
-		for(int x = 0; x < 9; ++x)
+		for(int x = 0; x < 9; ++x, ++p)
 		{
-			int v = board.trace[y][x];
+			int v = board.trace[p];
 			int bv = v != 0 ? 1 << (v - 1) : 0;
-			board.trace[y][x] = bv | (bv != 0 ? 512 : 0);
+			board.trace[p] = bv | (bv != 0 ? 512 : 0);
 			board.cols[x] |= bv;
 			board.rows[y] |= bv;
 			board.blocks[div3(y)][div3(x)] |= bv;
-			//printf("%u%c;", bitpos(board.trace[y][x] & 511), board.trace[y][x] & 512 ? '*' : ' ');
+			//printf("%u%c;", bitpos(board.trace[p] & 511), board.trace[p] & 512 ? '*' : ' ');
 		}
 	}
 }
-
-#define set_row(target, data) { for(int i = 0; i < 9; ++i) target[i] = data[i]; }
 
 int main(void)
 {
 	read_board_from_console();
 
-//	unsigned int r0[] = {0, 7, 0, 0, 0, 0, 0, 8, 0};
-//	set_row(board.trace[0], r0);
-//	unsigned int r1[] = {0, 3, 0, 7, 6, 2, 0, 0, 1};
-//	set_row(board.trace[1], r1);
-//	unsigned int r2[] = {0, 0, 1, 9, 8, 0, 0, 0, 0};
-//	set_row(board.trace[2], r2);
-//	unsigned int r3[] = {1, 0, 0, 0, 0, 0, 9, 0, 0};
-//	set_row(board.trace[3], r3);
-//	unsigned int r4[] = {8, 0, 3, 0, 0, 0, 1, 0, 2};
-//	set_row(board.trace[4], r4);
-//	unsigned int r5[] = {0, 0, 6, 0, 0, 0, 0, 0, 8};
-//	set_row(board.trace[5], r5);
-//	unsigned int r6[] = {0, 0, 0, 0, 3, 1, 6, 0, 0};
-//	set_row(board.trace[6], r6);
-//	unsigned int r7[] = {5, 0, 0, 2, 4, 9, 0, 1, 0};
-//	set_row(board.trace[7], r7);
-//	unsigned int r8[] = {0, 1, 0, 0, 0, 0, 0, 9, 0};
-//	set_row(board.trace[8], r8);
-
 	preprocess();
 
-	process(0, 0);
+	process(0);
 
 	return EXIT_SUCCESS;
 }
