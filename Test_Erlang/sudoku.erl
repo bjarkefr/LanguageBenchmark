dac9b1095207c9b5b149b277d4bba2775272ece9
Bjarke <bjarke@localhost>
2013-12-28 12:13:13 +0100

    Completed erlang version. There may still be some optimizations to do...

--- Test_Erlang/sudoku.erl
+++ Test_Erlang/sudoku.erl
@@ -1,31 +1,21 @@
 -module(sudoku).
 -export([main/0]).
-%%-compile({inline, [board_height/0, board_width/0, max_position/0, dummy_number/0, min_number/0, numbers/0]}).
+-compile({inline, [board_width/0, dummy_number/0, min_number/0, numbers/0, board_row/1, board_column/1, board_box_top/1, board_box_left/1, check_for_number/3, mapnth/3, setnth/3, update_board/3, board_position_assigned/2, for/3, evaluate_board_setup/2, build/3]}).
+-compile(inline_list_funcs).
+-compile(no_line_info).
 
 %%board_height() -> 9.
 board_width() -> 9.
 %%max_position() -> board_height() * board_width().
 
 dummy_number() -> 0.
-%%min_number() -> 1.
-%%numbers() -> 9.
+min_number() -> 1.
+numbers() -> 9.
 
 print_board([]) -> ok;
 print_board([Row | Rest]) ->
     io:format("~w ~w ~w ~w ~w ~w ~w ~w ~w ~n", Row),
     print_board(Rest).
-
-main() ->
-    Board = [[0,0,0,0,0,0,0,0,0],
-	    [0,0,0,0,0,0,0,0,0],
-	    [0,0,0,0,0,0,0,0,0],
-	    [0,0,0,0,0,0,0,0,0],
-	    [0,0,0,0,0,0,0,0,0],
-	    [0,0,0,0,0,0,0,0,0],
-	    [0,0,0,0,0,0,0,0,0],
-	    [0,0,0,0,0,0,0,0,0],
-	    [0,0,0,0,0,0,0,0,0]],
-    print_board(Board).
 
 board_row(Position) -> ((Position - 1) div board_width() + 1).
 
@@ -74,61 +64,43 @@ update_board(Board, Position, Number) ->
 board_position_assigned(Board, Position) ->
 	lists:nth(board_column(Position), lists:nth(board_row(Position), Board)) /= dummy_number().
 
+for(I, Max, _) when I == Max -> ok;
+for(I, Max, Fun) -> Fun(I), for(I + 1, Max, Fun). %% io:format("success ~w ~n", [I]), 
+
 evaluate_board_setup(Board, 82) -> print_board(Board);
 evaluate_board_setup(Board, Position) ->
 	Position_assigned = board_position_assigned(Board, Position),
 	if
 		Position_assigned -> evaluate_board_setup(Board, Position + 1);
 		true ->
-			
+			for(min_number(), min_number() + numbers(), fun(N) ->
+				PlacementValid = check_for_number(Board, Position, N),
+				if
+					PlacementValid ->
+						NewBoard = update_board(Board, Position, N),
+						evaluate_board_setup(NewBoard, Position + 1);
+					true -> ok
+				end
+			end)
 	end.
 
-
-
-%%void evaluate_board_setup(ref board board, in int position)
-%%{
-%%	if(position == MAX_POSITION)
-%%	{
-%%		print_board(board);
-%%		return;
-%%	}
-%%
-%%	if(board_position_assigned(board, position))
-%%		evaluate_board_setup(board, position + 1);
-%%	else
-%%	{
-%%		for(int number = MIN_NUMBER; number < (MIN_NUMBER + NUMBERS); ++number)
-%%			if(check_for_number(board, position, number))
-%%			{
-%%				update_board(board, position, number);
-%%				evaluate_board_setup(board, position + 1);
-%%			}
-%%		update_board(board, position, DUMMY_NUMBER);
-%%	}
-%%}
-%%
-%%pure nothrow void setup_board(ref board board)
-%%{
-%%	for(int x = 0; x < BOARD_WIDTH; ++x)
-%%		for(int y = 0; y < BOARD_HEIGHT; ++y)
-%%			board[y][x] = DUMMY_NUMBER;
-%%}
-%%
-%%pure nothrow void setup_test_board(ref board board)
-%%{
-%%	for(int pos = 0; pos < MAX_POSITION; ++pos)
-%%		board[board_row(pos)][board_column(pos)] = pos;
-%%}
-%%
-%%void read_board_from_console(ref board board)
-%%{
-%%	for(int pos = 0; pos < MAX_POSITION; ++pos)
-%%		readf(" %s", &board[board_row(pos)][board_column(pos)]);
-%%}
-%%
-%%void main()
-%%{
-%%	board board;
-%%	read_board_from_console(board);
-%%	evaluate_board_setup(board, 0);
-%%}
+setup_board() ->
+	[[0,0,0,0,0,0,0,0,0],
+	    [0,0,0,0,0,0,0,0,0],
+	    [0,0,0,0,0,0,0,0,0],
+	    [0,0,0,0,0,0,0,0,0],
+	    [0,0,0,0,0,0,0,0,0],
+	    [0,0,0,0,0,0,0,0,0],
+	    [0,0,0,0,0,0,0,0,0],
+	    [0,0,0,0,0,0,0,0,0],
+	    [0,0,0,0,0,0,0,0,0]].
+	
+build(I, Max, _) when I == Max -> [];
+build(I, Max, Fun) -> [Fun(I) | build(I + 1, Max, Fun)].
+	
+read_board_from_console() ->
+	build(1, 10, fun(_) -> element(2, io:fread("", "~d~d~d~d~d~d~d~d~d")) end).
+	
+main() ->
+	Board = read_board_from_console(),
+    evaluate_board_setup(Board, 1).
